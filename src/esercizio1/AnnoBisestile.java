dfebcf7ff1bee19380e0beae47ecdc6c19d8aa50
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-11-19 15:26:39 +0100

    change functions

--- src/esercizio1/AnnoBisestile.java
+++ src/esercizio1/AnnoBisestile.java
@@ -5,15 +5,9 @@ public class AnnoBisestile {
 
     // Metodo statico per verificare se un anno è bisestile
     public static boolean annoBisestile(int anno) {
-        // Un anno è bisestile se è divisibile per 4
-        if (anno % 4 == 0) {
-            // Se l'anno è divisibile per 100, è bisestile solo se è divisibile anche per 400
-            if (anno % 100 == 0) {
-                return anno % 400 == 0; // Restituisce true se divisibile per 400, altrimenti false
-            }
-            return true; // Se non è divisibile per 100 ma lo è per 4, è bisestile
-        }
-        return false; // Se non è divisibile per 4, non è bisestile
+
+        return (anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0);
+
     }
 
     // Metodo principale che esegue il programma
